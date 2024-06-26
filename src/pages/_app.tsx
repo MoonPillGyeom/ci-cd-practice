1efc713163667930f34bfe6207e2745a8ca4b92d
MoonPillGyeom <ansvkf0820@naver.com>
2024-06-26 15:52:18 +0900

    custom app ci-cd pratice

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,40 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+// import "@/styles/globals.css";
+// import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />;
+// }
+
+// import { SpeedInsights } from "@vercel/speed-insights/next";
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <head>
+//         <title>Next.js</title>
+//       </head>
+//       <body>
+//         {children}
+//         <SpeedInsights />
+//       </body>
+//     </html>
+//   );
+// }
+
+import type { AppProps } from "next/app";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
 }
+
+export default MyApp;
